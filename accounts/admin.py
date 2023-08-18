f0aea17c83644e03b2867513a417be44c7bc8cd0
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-08-18 13:50:29 +0100

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -20,8 +20,19 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Userpassword)
+class UserpasswordAdmin(admin.ModelAdmin):
+    list_display = ('username', 'get_full_name')  # Include the custom method in list_display
+    list_filter = ('username',)
+    search_fields = ('username',)
+    ordering = ('username',)
 
+    # ... other admin customization ...
+
+    def get_full_name(self, obj):
+        return f"{obj.username}"  # You can customize this to generate the full name
+    get_full_name.short_description = 'Full Name'  # Set the column header in the admin list view
+
+admin.site.register(Userpassword, UserpasswordAdmin)
 
 @admin.register(AccountDetails)
 class AccountDetailsAdmin(admin.ModelAdmin):
